344e17acc8a7cab78ad106276cbd4f12e16a8cee
Łukasz <lukaszfilo@student.agh.edu.pl>
2023-11-23 10:32:11 +0100

    buttons added

--- main_window.py
+++ main_window.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import (
     QApplication,
     QGridLayout,
@@ -8,6 +9,8 @@ from PyQt6.QtWidgets import (
     QPushButton,
     QVBoxLayout,
     QWidget,
+    QToolBar,
+    QStatusBar
 )
 
 WINDOW_WIDTH = 400
@@ -34,6 +37,43 @@ class CalcMainWindow(QMainWindow):
         centralWidget.setLayout(self.generalLayout)
         self.setCentralWidget(centralWidget)
 
+        # Sample button
+        sample_button = QAction("&Sample button", self)
+        sample_button.setStatusTip("This is Sample button")
+        sample_button.triggered.connect(self.SampleButtonClick)
+
+        # Plot calc button
+        plot_calc_button = QAction("&Draw plot", self)
+        plot_calc_button.setStatusTip("Open plot calc")
+        plot_calc_button.triggered.connect(self.DrawPlotWindow)
+
+        #Exit Button
+        exit_button = QAction("&Quit", self)
+        exit_button.setStatusTip("Quit app")
+        exit_button.triggered.connect(QApplication.instance().quit)
+
+        # Toolbar
+        toolbar = QToolBar("Main toolbar")
+        toolbar.setMovable(False)
+        self.addToolBar(toolbar)
+
+        # Adding buttons to toolbar
+        toolbar.addAction(sample_button)
+        toolbar.addAction(plot_calc_button)
+        toolbar.addAction(exit_button)
+
+        # Create statusbar
+        self.setStatusBar(QStatusBar(self))
+
+
+    def DrawPlotWindow(self):
+        print('Plotter opened')
+
+    def SampleButtonClick(self, s):
+        print('button clicked')
+    
+    def quitApp(self):
+        QApplication.instance().quit
 
 def main():
     calcApp = QApplication(sys.argv)
